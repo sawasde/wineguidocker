17d981892b12c2dcc9a28abf7c87bcec82bae5a0
Alejandro Aguirre Soto <alasoto@bancolombia.com.co>
2022-08-04 14:36:24 -0500

    Modify to run any exe not only metatrader

--- app/run.sh
+++ app/run.sh
@@ -10,4 +10,4 @@ while read cookie; do
 done < /mcookie/cookies.txt
 
 # Start MetaTrader
-wine explorer /desktop=L4D2,"$GEOMETRY" terminal.exe
+wine explorer /desktop=L4D2,"$GEOMETRY" "$EXECUTABLE"
