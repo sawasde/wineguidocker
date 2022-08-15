f91ecb73825e518dd5bc038da6ce6b4ad033897a
Ale Aguirre Soto <aguirre.soto.alejandro@gmail.com>
2022-08-15 18:20:07 -0500

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -9,5 +9,5 @@ while read cookie; do
   xauth add $cookie
 done < /mcookie/cookies.txt
 
-# Start Application
+# Start Application. e.g wine
 wine explorer /desktop=L4D2,"$GEOMETRY" "bin/$EXECUTABLE"
